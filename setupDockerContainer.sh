9ba882b386639573181a22d5e640ac08b1d11df3
Brandon Landry <sirjava@gmail.com>
2015-07-13 09:35:44 -0700

    can't install git from a script fetched w/ git .... /face_palm

--- setupDockerContainer.sh
+++ setupDockerContainer.sh
@@ -2,13 +2,12 @@
 
 ### Setup the container with ruby
 # ensure ruby is installed
+apt-get install curl
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 source /usr/local/rvm/scripts/rvm
 echo "source /usr/local/rvm/scripts/rvm" >> ~/.bashrc
 rvm reload
-
-apt-get install curl git
 
 git clone https://github.com/hotpocket/my-nginx-install.git
 cd my-nginx-install
